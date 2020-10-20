3dc9345992041127d71a573e3036fcc70c5f16e2
hanta1008 <55986265+hanta1008@users.noreply.github.com>
2020-10-20 14:38:31 +0900

    * AudioManager 추가

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine.Audio;
+using System;
+using UnityEngine;
+
+[System.Serializable]
+public class Sound
+{
+    public string name;
+
+    public AudioClip clip;
+
+    [Range(0f, 1f)]
+    public float volume;
+    [Range(0.1f, 3f)]
+    public float pitch;
+    public bool loop;
+    [HideInInspector]
+    public AudioSource source;
+}
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+
+    private static AudioManager instance;
+    public static AudioManager Instance
+    {
+        get
+        {
+            if (instance != null)
+                return instance;
+            return null;
+        }
+    }
+
+    private void Awake()
+    {
+        if (instance != null)
+            Destroy(gameObject);
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+
+        foreach (Sound sound in sounds)
+        {
+            sound.source = gameObject.AddComponent<AudioSource>();
+            sound.source.clip = sound.clip;
+            sound.source.volume = sound.volume;
+            sound.source.pitch = sound.pitch;
+            sound.source.loop = sound.loop;
+        }
+    }
+
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+
+        if (s == null)
+            return;
+        s.source.Play();
+    }
+}
